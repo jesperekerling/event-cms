2682b789266bbdd8787d306fe00ae008c3aef08d
Leila <leilaabdulahad@hotmail.com>
2024-05-12 11:25:18 +0200

    implementation of clerk

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,10 @@
+
+function AuthLayout({ children }) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+          {children}
+      </div>
+    )
+  }
+  
+  export default AuthLayout
