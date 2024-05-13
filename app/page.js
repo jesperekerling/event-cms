99cc8417a40c3b6be206aa3889041958bb65d855
Jesper Ekerling <jesper@forme.se>
2024-05-13 16:17:35 +0200

    Design update

--- app/page.js
+++ app/page.js
@@ -48,8 +48,15 @@ export default function Home() {
               <span className="text-gray-500 text-sm font-semibold">99 USD</span>
             </div>
             <div className="flex">
-              <span className="text-sm">
+              <span className="text-sm flex-1">
                 34 spots left
+              </span>
+              <span>
+                <a href="#" className="text-sm">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 md:w-5 md:h-5 inline align-middle">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                  </svg> <span className="align-bottom">Edit</span>
+                </a>
               </span>
             </div>
           </article>
