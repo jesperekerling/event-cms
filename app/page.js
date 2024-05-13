122a6d8f20a7bba30fa3317ee43be8582db1c61c
Jesper Ekerling <jesper@forme.se>
2024-05-13 13:41:55 +0200

    Adding top menu and footer components

--- app/page.js
+++ app/page.js
@@ -3,15 +3,16 @@ import { SignInButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main >
-      <h1>Hello, World!</h1>
-        <SignedOut>
-            <SignInButton mode="modal" />
-          </SignedOut>
-          <SignedIn>
-            <UserButton /> 
-          </SignedIn>
+    <main>
       
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton /> 
+      </SignedIn>
+
     </main>
   );
 }
