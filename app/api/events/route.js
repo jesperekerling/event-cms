c424b647b2827964a965e5b4e499fe04649a264d
lottiger <linda.lottiger@student.kyh.se>
2024-05-21 14:42:02 +0200

    s

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -2,26 +2,6 @@ import { api } from "@/convex/_generated/api";
 import { fetchMutation, fetchQuery } from "convex/nextjs";
 import { NextResponse } from "next/server";
 
-// export async function POST(request) {
-//   try {
-//     const { title, description, date, location, price, seats, imageId } = request.body;
-
-//     const newEvent = await fetchMutation(api.events.createEvent, {
-//       title,
-//       description,
-//       date,
-//       location,
-//       price,
-//       seats,
-//       imageId,
-//     });
-
-//     return NextResponse.json(newEvent, { status: 201 });
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json({ message: 'An error occurred while creating the event.' }, { status: 500 });
-//   }
-// }
 
 export async function GET(request) {
 
